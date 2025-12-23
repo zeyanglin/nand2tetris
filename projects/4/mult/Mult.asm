2ecc30c07a23293561e0da51abeb73ee42e8b7fc
zeyanglin <linzeyang20@gmail.com>
2025-12-23 15:45:48 +0800

    Module 4 Finished

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,40 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+// Pseudo code:
+// initialize sum = 0;
+// while(R1>0){
+//     R1--;
+//     sum=sum+R0;
+// }                 
+	
+// initialize sum=0;
+@2
+M=0
+
+// 循环开始位置
+(LOOP)
+
+// 循环条件
+@1
+D=M
+@END
+D;JLE
+
+// R1 = R1 - 1
+D=D-1
+@1
+M=D
+
+// sum=sum+R0;
+@0
+D=M // D=R0
+@2
+M=D+M
+
+// 无条件跳转到（LOOP）所在位置
+@LOOP
+0;JMP
+
+(END)
